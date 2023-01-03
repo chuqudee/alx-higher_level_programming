274d28a571263dca4152d8c6cc98cc7b373ca3c1
chuqudee <niky4u2nv@gmail.com>
2023-01-03 05:29:41 +0100

    task updated

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
 	unsigned int n;
-
+	/* number of nodes */
 	current = h;
 	n = 0;
 	while (current != NULL)
@@ -35,9 +35,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	new->next = *head;
 	*head = new;
@@ -60,4 +58,3 @@ void free_listint(listint_t *head)
 		free(current);
 	}
 }
-
