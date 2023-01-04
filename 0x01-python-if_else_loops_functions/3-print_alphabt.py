0b4c91069946215d1c0a473f7734900432ef19ac
chuqudee <niky4u2nv@gmail.com>
2023-01-04 05:54:40 +0100

    task updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('z') + 1):
-    if alpha == ord('q') or alpha == ord('e'):
-        continue
-    print("{:c}".format(alpha), end='')
+for i in range(97, 123):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print("{}".format(chr(i)), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i == 8:
-            print("{}{}".format(i, j))
+
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if (not (x == 8 and y == 9)):
+            print("{}{}".format(x, y), end=", ")
         else:
-            print("{}{}".format(i, j), end=", ")
+            print("{}{}".format(x, y))
